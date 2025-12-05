94a0a651fb1e725e5dfca4af3bd11bb3d0daf724
Olivier Wenger <olivier@3ding.ch>
2025-12-05 23:22:02 +0100

    debug

--- components/rotary_encoder_custom/rotary_encoder_custom.h
+++ components/rotary_encoder_custom/rotary_encoder_custom.h
@@ -15,8 +15,8 @@ class RotaryEncoderCustom : public Component, public sensor::Sensor {
   // These take the pointer types that esphome::gpio_pin_expression returns
   void set_pin_a(GPIOPin *pin_a) { this->pin_a_ = pin_a; }
   void set_pin_b(GPIOPin *pin_b) { this->pin_b_ = pin_b; }
-  void set_min_value(int *min_value) {this->min_value = min_value;}
-  void set_max_value(int *max_value) {this->max_value = max_value;}
+  void set_min_value(int *min_value) {this->min_value_ = min_value;}
+  void set_max_value(int *max_value) {this->max_value_ = max_value;}
   
   void set_publish_initial_value(bool v) { this->publish_initial_value_ = v; }
 
