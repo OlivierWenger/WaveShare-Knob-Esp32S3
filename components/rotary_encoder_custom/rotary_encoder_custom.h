56bb3f516e87b3460f6e00a8c26c619b8adeb458
Olivier Wenger <olivier@3ding.ch>
2025-12-05 22:19:00 +0100

    debounce 0

--- components/rotary_encoder_custom/rotary_encoder_custom.h
+++ components/rotary_encoder_custom/rotary_encoder_custom.h
@@ -8,7 +8,7 @@ namespace esphome {
 namespace rotary_encoder_custom {
 
 // If you still miss steps at high speed, raise this to 6 or 7
-static constexpr uint8_t DEBOUNCE_TICKS = 7;
+static constexpr uint8_t DEBOUNCE_TICKS = 0;
 
 class RotaryEncoderCustom : public Component, public sensor::Sensor {
  public:
